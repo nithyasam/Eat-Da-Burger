9a06cfcd317e089ee8de411ba2be03a44a713fc7
Nithya Sam <nithyasam@nithyas-air.gateway.pace.com>
2017-08-01 14:41:23 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured, orderdate) VALUES("Bacon cheese burger", true, CURRENT_TIMESTAMP);
+INSERT INTO burgers(burger_name, devoured, orderdate) VALUES("veggie burger", true, CURRENT_TIMESTAMP);
+INSERT INTO burgers(burger_name, devoured, orderdate) VALUES("Chicken burger", true, CURRENT_TIMESTAMP);
