70f381d9bd98f584f71394cf80efa6662fac0fc0
Nithya Sam <nithyasam@nithyas-air.gateway.pace.com>
2017-08-05 03:27:43 -0500

    added the mvc pattern

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,17 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
-  database: "moviePlannerDB"
+  database: "burgers_db"
 });
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 
 module.exports = connection;
