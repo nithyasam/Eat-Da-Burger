0a8651cf26d522079f5f4a3ea7574f6ffa2fb65e
Nithya Sam <nithyasam@nithyas-air.gateway.pace.com>
2017-08-06 22:49:21 -0500

    updated README

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE burgers
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(255) NOT NULL,
 devoured BOOLEAN default 0,
-orderdate TIMESTAMP,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
